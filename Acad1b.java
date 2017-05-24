db59fb2fd0f9fa65a7972e709acdfb92e5a2e7e1
8608311411IIIT <sanjeevkumar181998@gmail.com>
2017-05-24 18:25:48 +0530

    Add files via upload

---
+++ Acad1b.java
@@ -0,0 +1,16 @@
+public class Acad1b
+{
+public static void main(String []args){
+  int a=20,b=10;
+  b=a-- - --a;
+  System.out.println("int b="+b);
+ int c=a--;
+  System.out.println("int c="+c);
+ int d=a>>2;
+  System.out.println("int d="+d);
+ int e=a&b;
+  System.out.println("int e="+e); 
+  
+}
+
+}
